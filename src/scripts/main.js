7765156e89caa7476abb6bd32f3557193c4dff99
Yan Araujo <saresende555@gmail.com>
2023-09-28 13:00:12 -0300

    corrigindo a data do evento

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,6 @@
 AOS.init();
 
-const dataDoEvento = new Date("Dec 12, 2022 19:00:00");
+const dataDoEvento = new Date("Dec 12, 2024 19:00:00");
 const timeStampDoEvento = dataDoEvento.getTime();
 
 const contaAsHoras = setInterval(function () {
